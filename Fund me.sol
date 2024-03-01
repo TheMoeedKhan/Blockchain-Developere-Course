eb0fda75e68572397153f3c6a4b4a45206c6019d
Moeed Khan <70565490+TheMoeedKhan@users.noreply.github.com>
2024-03-02 02:19:46 +0500

    Update Fund me.sol

--- Fund me.sol
+++ Fund me.sol
@@ -2,16 +2,30 @@
 
 pragma solidity ^0.8.0;
 
-contract Fundme{
-    
-    uint public minimumUSD = 5; 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+contract Fundme {
+    uint256 public minimumUSD = 5;
 
-    function fund() public payable {    //call to send money to our contract
+    function fund() public payable {
+        //call to send money to our contract
 
-    require(msg.value >= minimumUSD, "Transaction failed");
+        require(msg.value >= minimumUSD, "Transaction failed");
     }
 
-    // function withdraw() public {      //to withdraw funds
+    // function withdraw() public {}    //to withdraw funds
 
-    // }
+    function getPrice() public view returns (uint256) {
+        // Address: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        // ABI: 'price' is Price of Eth in USD
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+        return uint (price * 1e10);
+    }
+
+    function GetConversionRate() public {}
+
+    function getVersion() public view returns (uint256){
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return priceFeed.version();
+    }
 }
