9f1d2102089c45a19c2890dfeb28c53837b35876
Moeed Khan <70565490+TheMoeedKhan@users.noreply.github.com>
2024-03-21 01:45:16 +0500

    Update Fund me.sol

--- Fund me.sol
+++ Fund me.sol
@@ -4,25 +4,32 @@ pragma solidity ^0.8.0;
 
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 contract Fundme {
-    uint256 public minimumUSD = 5;
+    uint256 public minimumUSD = 5e18;
 
     function fund() public payable {
         //call to send money to our contract
 
-        require(msg.value >= minimumUSD, "Transaction failed");
+        require(GetConversionRate(msg.value) >= minimumUSD, "Transaction failed: Did'nt Send Enough ETH");
     }
 
     // function withdraw() public {}    //to withdraw funds
 
     function getPrice() public view returns (uint256) {
         // Address: 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        // ABI: 'price' is Price of Eth in USD
+        // ABI: 
+        //'price' is Price of Eth in USD
+
+
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (, int256 price, , , ) = priceFeed.latestRoundData();
-        return uint (price * 1e10);
+        (, int256 price,,,) = priceFeed.latestRoundData();
+        return uint256 (price * 1e10);
     }
 
-    function GetConversionRate() public {}
+    function GetConversionRate(uint256 ethAmount) public view returns(uint256) {
+        uint256  ethPrice = getPrice();
+        uint256  ethAmountInUSD = (ethPrice * ethAmount) / 1e18;  
+        return ethAmountInUSD;
+    }
 
     function getVersion() public view returns (uint256){
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
